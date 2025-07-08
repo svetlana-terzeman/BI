7466a3f2fbace175b1d6468c15bc347cfb664e0a
svetlana-terzeman <terzeman01@gmail.com>
2025-07-08 18:19:12 +0300

    обновление .env

--- settings/constants.py
+++ settings/constants.py
@@ -1,16 +1,18 @@
 import os
 
-HOST       = 'db'
-PORT     =  5432
-DB       = 'BI'
-USERNAME = 'bitask_user'
-PASSWD   = 'bitask_pass'
-RABBITMQ_HOST = 'rabbitmq'
-RABBITMQ_USER = 'admin'
-RABBITMQ_PASS = 'mq_pass'
-tablename     = 'bi__task_register'
-retry_delay   = 5  # время ожидания подключения к RabbitMQ
-max_retries   = 15 # максимальное кол-во попыток подключения к RabbitMQ
+HOST     =  os.getenv('DB_HOST', 'db')
+DB       =  os.getenv('DB_NAME', 'BI')
+USERNAME =  os.getenv('DB_USER', 'bitask_user')
+PASSWD   =  os.getenv('DB_PASS', 'bitask_pass')
+tablename =  os.getenv('BI_tablename', 'bi__task_register')
+PORT      =  int(os.getenv('DB_PORT', 5432))
+
+RABBITMQ_HOST = os.getenv('RABBITMQ_HOST', 'rabbitmq')
+RABBITMQ_USER = os.getenv('RABBITMQ_DEFAULT_USER', 'admin')
+RABBITMQ_PASS = os.getenv('RABBITMQ_DEFAULT_PASS', 'mq_pass')
+
+retry_delay   = int(os.getenv('retry_delay', 5))  # время ожидания подключения к RabbitMQ
+max_retries   = int(os.getenv('max_retries', 15)) # максимальное кол-во попыток подключения к RabbitMQ
 
 scheme_forms = {'LTV': {
                             'CURRDATE': {
